59fecaec25fabfce2ad1048d6cb04c1057fb0b2e
statjhw <136469229+statjhw@users.noreply.github.com>
2025-01-12 15:41:04 +0900

    Feat: Add solution for 2751

---
+++ 백준_solved.ac/class2/2751_수정렬하기2.py
@@ -0,0 +1,13 @@
+#2751_수 정렬하기 2
+import sys
+import heapq
+
+N = int(sys.stdin.readline().strip())
+
+heap = []
+
+for _ in range(N) :
+    heapq.heappush(heap, int(sys.stdin.readline().strip()))
+
+for _ in range(N) :
+    print(heapq.heappop(heap))
