d4b0690ae68afb35ce3e61ce9708eab77e94ca78
statjhw <136469229+statjhw@users.noreply.github.com>
2025-01-15 16:46:56 +0900

    Feat: Add solution for 9012

--- 백준_solved.ac/class2/9012_괄호.py
+++ 백준_solved.ac/class2/9012_괄호.py
@@ -1,2 +1,27 @@
 #9012_괄호
 #스택
+from collections import deque
+
+N = int(input())
+
+for _ in range(N) :
+    vps = input()
+    check_stack = deque()
+    
+    flag = True
+    for x in vps :
+        if x == "(" :
+            check_stack.append(x)
+        elif x == ")" :
+            if len(check_stack) == 0 :
+                flag = False
+                break
+            else :
+                if check_stack.pop() != "(" :
+                    flag = False
+                    break
+    
+    if flag and len(check_stack) == 0 :
+        print("YES")
+    else :
+        print("NO")
