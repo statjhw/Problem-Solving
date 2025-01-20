edd8cb35ee7e7baace9dac1b967e2dc6dd517c62
general <we1905@naver.com>
2025-01-20 16:39:18 +0900

    Feat: Add solution for 1654

--- 백준_solved.ac/class2/1654_랜선자르기.py
+++ 백준_solved.ac/class2/1654_랜선자르기.py
@@ -1,6 +1,7 @@
 #1654_랜선 자르기
-
+#시간복잡도 O(nlogn)
 import sys
+import math
 input = sys.stdin.readline
 
 
@@ -9,32 +10,21 @@ lst = []
 for _ in range(K) :
     lst.append(int(input()))
 
-lst.sort(reverse=True)
+lst.sort()
 
-max_length = lst[1]
-idx = 1
+start, end = 1, max(lst)
+result = 0
 
-while 1 :
-    flag = False
-    total = 0
-    for x in lst :
-        if x >= max_length :
-            total += x // max_length
-            if total >= N :
-                flag = True
-                break
+while start <= end :
+    value = 0
+    mid = (start + end) // 2
     
-    if flag :
-        break
+    for x in lst :
+        value += x // mid
+    if value < N :
+        end = mid - 1 
     else :
-        if idx <= len(lst) - 1 :
-            if max_length//2 > lst[idx] :
-                max_length = max_length // 2 
-            else :
-                max_length = lst[idx] 
-                idx += 1
-        else :
-            max_length = max_length - 1
-print(max_length)
+        result = mid 
+        start = mid + 1
 
-
+print(result)
