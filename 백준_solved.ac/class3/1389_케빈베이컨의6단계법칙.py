5a7f88423082b6ce86e11535c96881c97f550b8a
statjhw <136469229+statjhw@users.noreply.github.com>
2025-02-17 13:31:26 +0900

    Feat: Add solution for 1389

--- 백준_solved.ac/class3/1389_케빈베이컨의6단계법칙.py
+++ 백준_solved.ac/class3/1389_케빈베이컨의6단계법칙.py
@@ -5,43 +5,46 @@ import sys
 from collections import deque, defaultdict
 input = sys.stdin.readline
 
-#bfs인데 최단경로를 구해야 하는데 모든 경우에 대해서 구해야 함
-#L_i의 리스트를 이용해야 함
+def bfs_min_path(graph, start, end) -> int :
+    """return : start와 end가 거쳐야하는 길의 크기의 최솟값"""
+    queue = deque([start])
+    distance = {start: 0}
 
-def bfs(graph, visit, node, end) :
-    path = [[node]]
-    visit[node] = True
-    i = 0
-    while len(path[i]) != 0 :
-        path.append([])
-        for n in path[i] :
-            for v in graph[n] :
-                if not visit[v] :
-                    path[i+1].append(v)
-                    visit[v] = True
-                    if v == end :
-                        return i+1
+    while queue :
+        node = queue.popleft()
+        for neighbor in graph[node] :
+            if neighbor not in distance :
+                distance[neighbor] = distance[node] + 1
+                queue.append(neighbor)
 
-        i = i+1
-    return None
+    return distance.get(end, -1)
+
 
 
 N, M = map(int, input().split())
 graph = defaultdict(list)
+
 for _ in range(M) :
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-
-result = [-1 for _ in range(N+1)]
+result = [0 for _ in range(N+1)]
 for i in range(1, N+1) :
-    r = 0
     for j in range(1, N+1) :
         if i == j :
             continue
         else :
-            visit = [False for _ in range(N + 1)]
-            r += bfs(graph, visit, i, j)
+            value = bfs_min_path(graph, i, j)
+            result[i] += value
 
-    result[i] = r
+
+min_idx = 1
+min_value = result[1]
+
+for i in range(2, N+1) :
+    if min_value > result[i] :
+        min_idx = i
+        min_value = result[i]
+
+print(min_idx)
