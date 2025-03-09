52bb567b1d494d48751f44190ac0e012c2fe530c
statjhw <136469229+statjhw@users.noreply.github.com>
2025-03-09 18:02:36 +0900

    Feat: Add solution for 16928

--- 백준_solved.ac/class3/16928_뱀과사다리게임.py
+++ 백준_solved.ac/class3/16928_뱀과사다리게임.py
@@ -1,20 +1,9 @@
 #16928_뱀과사다리게임
 #그래프, bfs
 
-from collections import defaultdict, deque
+from collections import deque
 import sys
-input = sys.stdin.readline
 
-N, M = map(int, input().split())
-game = dict()
-
-for _ in range(N) :
-    start, end = map(int, input().split())
-    game[start] = [True, end]
-
-for _ in range(M) :
-    start, end = map(int, input().split())
-    game[start] = [False, end]
 
 def bfs(game) :
     dice = [1, 2, 3, 4, 5, 6]
@@ -23,29 +12,25 @@ def bfs(game) :
 
     while queue :
         node = queue.popleft()
-        for x in dice :
-            next_node = node + x
-            if next_node <= 100 :
-                if next_node not in distances :
-                    if next_node in game :
-                        if game[next_node][0] :
-                            next_next_node = game[next_node][1]
-                            distances[next_node] = 1 + distances[node]
-                            distances[next_next_node] = 1 + distances[node]
-                            queue.append(next_next_node)
-                        else :
-                            next_next_node = game[next_node][1]
-                            if next_next_node not in distances :
-                                distances[next_node] = 1 + distances[node]
-                                distances[next_next_node] = 1 + distances[node]
-                                queue.append(next_next_node)
-                    else :
-                        distances[next_node] = 1 + distances[node]
-                        queue.append(next_node)
+        for i in dice :
+            next_node = node + i
+            if next_node in game :      #사다리나 뱀이 존재하는 경우
+                next_node = game[next_node][1]
+            if next_node <= 100 and next_node not in distances :
+                distances[next_node] = distances[node] + 1
+                queue.append(next_node)
 
     return distances[100]
 
+N, M = map(int, input().split())
+
+game = dict()
+for _ in range(N) :
+    start, end = map(int, input().split())
+    game[start] = (True, end)
+for _ in range(M) :
+    start, end = map(int, input().split())
+    game[start] = [False, end]
+
 result = bfs(game)
 print(result)
-
-
